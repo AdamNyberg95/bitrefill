bff0d8dd6434be29bbcc319d70490c67e343eaef
adam nyberg <adamnyberg@MacBook-Pro-som-tillhor-adam.local>
2024-09-23 08:56:09 +0300

    changes o navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css'; // Import the CSS file for Navbar
 import { BitrefillLogo, TrashcanIcon, CartIcon } from '../../assets/icons';
 import CartDropdown from '../CartDropdown/CartDropdown';
 import { useCart } from '../../context/CartContext';
+import { useAnimation } from 'framer-motion';
 const Navbar: React.FC = () => {
   const [isCartOpen, setCartOpen] = useState(false);
   const { cartItems } = useCart();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   const getTotalItemsCount = () => {
     return cartItems.reduce((total, item) => total + item.quantity, 0);
   };
   return (
-    <nav>
+    <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
       <ul
         style={{
           padding: '0',
+          margin: '0',
         }}
       >
         <BitrefillLogo />
